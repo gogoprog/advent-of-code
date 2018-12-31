44f1a8092e65ae9dffc49908e293f6d1ca4f5bf7
Gauthier Billot <gogoprog@gmail.com>
2018-12-31 14:11:36 +0100

    2015 19

--- 2015/19/main.cpp
+++ 2015/19/main.cpp
@@ -45,36 +45,13 @@ void parseMolecule(Molecule &result, const std::string input) {
     }
 }
 
-void process(const Replacements &replacements, const Molecule &inputMolecule, const Molecule &target, const int steps) {
-    for (size_t a{0}; a < inputMolecule.size(); ++a) {
-        auto &atom = inputMolecule[a];
-        auto it = replacements.find(atom);
-        if (it != std::end(replacements)) {
-            std::vector<Molecule> newMolecules{};
-            for (const auto &r : it->second) {
-                auto copied = inputMolecule;
-                auto p = copied.begin() + a;
-                copied.erase(p);
-                copied.insert(p, r.begin(), r.end());
+int minSize{10000};
+std::set<Molecule> processed;
 
-                if (copied.size() < target.size()) {
-                    newMolecules.push_back(copied);
-                } else if (copied.size() == target.size() && copied == target) {
-                    std::cout << target << std::endl;
-                    std::cout << steps << std::endl;
-                    exit(0);
-                }
-            }
+bool rprocess(const ReverseReplacements &revReplacements, const Molecule &current, const Molecule &target,
+              const int steps) {
+    processed.insert(current);
 
-            for (const auto &newMolecule : newMolecules) {
-                process(replacements, newMolecule, target, steps + 1);
-            }
-        }
-    }
-}
-
-void rprocess(const ReverseReplacements &revReplacements, const Molecule &current, const Molecule &target,
-              const int steps, int &minSteps) {
     for (const auto &rep : revReplacements) {
         auto it = current.begin();
         const auto &m = rep.first;
@@ -87,23 +64,24 @@ void rprocess(const ReverseReplacements &revReplacements, const Molecule &curren
 
             if (copied.size() == 1) {
                 if (copied == target) {
-
-                    if (steps < minSteps) {
-                        std::cout << steps << std::endl;
-                        minSteps = steps;
-                    }
-                    ++it;
-                    continue;
+                    std::cout << "part2: " << steps << std::endl;
+                    return true;
                 }
             }
 
             if (rep.second != "e") {
-                std::cout << copied << std::endl;
-                rprocess(revReplacements, copied, target, steps + 1, minSteps);
+                if (processed.find(copied) != processed.end()) {
+                    return false;
+                }
+                if (rprocess(revReplacements, copied, target, steps + 1)) {
+                    return true;
+                }
             }
             ++it;
         }
     }
+
+    return false;
 }
 
 void processFile(const std::string filename, const std::string input2) {
@@ -145,43 +123,33 @@ void processFile(const std::string filename, const std::string input2) {
                 copied.insert(p, r.begin(), r.end());
 
                 ++count;
-                if (allMolecules.find(copied) == allMolecules.end()) {
-                    allMolecules.insert(copied);
-                }
+                allMolecules.insert(copied);
             }
         }
     }
 
-    std::cout << allMolecules.size() << " / " << count << std::endl;
+    std::cout << "part1: " << allMolecules.size() << " / " << count << std::endl;
 
     Molecule start;
     parseMolecule(start, "e");
-    int minSteps{std::numeric_limits<int>::max()};
-    rprocess(revReplacements, inputMolecule, start, 1, minSteps);
-
-    if (minSteps != std::numeric_limits<int>::max()) {
-        std::cout << "Found: " << minSteps << std::endl;
-    } else {
-        std::cout << "Not found" << std::endl;
-    }
-
-    system("read");
+    minSize = 10000;
+    processed.clear();
+    rprocess(revReplacements, inputMolecule, start, 1);
 }
 
 int main() {
 
+    processFile("input2.txt", "HOH");
     processFile("input2.txt", "HOHOHO");
 
-    processFile("input.txt", "CRnPRnCaRnFArArFArAl");
+    auto input =
+        "CRnCaCaCaSiRnBPTiMgArSiRnSiRnMgArSiRnCaFArTiTiBSiThFYCaFArCaCaSiThCaPBSiThSiThCaCaPTiRnPBSiThRnFArArCaCaSiThCa"
+        "SiThSiRnMgArCaPTiBPRnFArSiThCaSiRnFArBCaSiRnCaPRnFArPMgYCaFArCaPTiTiTiBPBSiThCaPTiBPBSiRnFArBPBSiRnCaFArBPRnSi"
+        "RnFArRnSiRnBFArCaFArCaCaCaSiThSiThCaCaPBPTiTiRnFArCaPTiBSiAlArPBCaCaCaCaCaSiRnMgArCaSiThFArThCaSiThCaSiRnCaFYC"
+        "aSiRnFYFArFArCaSiRnFYFArCaSiRnBPMgArSiThPRnFArCaSiRnFArTiRnSiRnFYFArCaSiRnBFArCaSiRnTiMgArSiThCaSiThCaFArPRnFA"
+        "rSiRnFArTiTiTiTiBCaCaSiRnCaCaFYFArSiThCaPTiBPTiBCaSiThSiRnMgArCaF";
 
-    processFile("input.txt", "CRnCaCaCaSiRnBPTiMgArSiRnSiRnMgArSiRnCaFArTiTiBSiThFYCaFArCaCaSiThCaPB"
-                             "SiThSiThCaCaPTiRnPBSiThRnFArArCaCaSiThCaSiThSiRnMgArCaPTiBPRnFArSiThCa"
-                             "SiRnFArBCaSiRnCaPRnFArPMgYCaFArCaPTiTiTiBPBSiThCaPTiBPBSiRnFArBPBSiRnC"
-                             "aFArBPRnSiRnFArRnSiRnBFArCaFArCaCaCaSiThSiThCaCaPBPTiTiRnFArCaPTiBSiAl"
-                             "ArPBCaCaCaCaCaSiRnMgArCaSiThFArThCaSiThCaSiRnCaFYCaSiRnFYFArFArCaSiRnF"
-                             "YFArCaSiRnBPMgArSiThPRnFArCaSiRnFArTiRnSiRnFYFArCaSiRnBFArCaSiRnTiMgAr"
-                             "SiThCaSiThCaFArPRnFArSiRnFArTiTiTiTiBCaCaSiRnCaCaFYFArSiThCaPTiBPTiBCa"
-                             "SiThSiRnMgArCaF");
+    processFile("input.txt", input);
 
     return 0;
 }
