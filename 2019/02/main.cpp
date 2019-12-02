d66003f41705c6fc0cfae901c79d0b85851b87bc
Gauthier Billot <gogoprog@gmail.com>
2019-12-02 21:35:04 +0100

    Clean 2019 02

--- 2019/02/main.cpp
+++ 2019/02/main.cpp
@@ -1,62 +1,84 @@
 #include "../../common.h"
 
+using Program = Vector<int>;
+
+struct Machine {
+    Program initialProgram;
+    Program program;
+    int cursor{0};
+
+    void load(const String line) {
+        auto inputs = splitString(line, ',');
+
+        initialProgram.clear();
+
+        for (auto &input : inputs) {
+            initialProgram.push_back(std::stoi(input));
+        }
+    }
+
+    void reset() {
+        cursor = 0;
+        program = initialProgram;
+    }
+
+    int run(const int noun, const int verb) {
+        reset();
+
+        program[1] = noun;
+        program[2] = verb;
+
+        while (cursor < program.size()) {
+            auto i = program[cursor];
+
+            switch (i) {
+                case 1:
+                case 2: {
+                    auto a = program[cursor + 1];
+                    auto b = program[cursor + 2];
+                    auto c = program[cursor + 3];
+
+                    int r;
+
+                    if (i == 1) {
+                        r = program[a] + program[b];
+                    } else {
+                        r = program[a] * program[b];
+                    }
+
+                    program[c] = r;
+
+                    cursor += 4;
+
+                } break;
+
+                case 99:
+                    return program[0];
+                    break;
+                default:
+                    cursor++;
+            }
+
+            cursor %= program.size();
+        }
+
+        return -1;
+    }
+};
+
 void process(const String filename) {
     log << "Processing " << filename << endl;
     auto lines = getFileLines(filename);
-    auto r = 0;
+    Machine machine;
 
-    auto items = splitString(lines[0], ',');
+    machine.load(lines[0]);
+
+    log << machine.run(12, 2) << endl;
 
     for (int noun = 0; noun < 99; noun++) {
         for (int verb = 0; verb < 99; verb++) {
-            Vector<int> values;
 
-            for (int i = 0; i < items.size(); i++) {
-                auto item = items[i];
-                InputStringStream iss{item};
-                int a;
-                iss >> a;
-                values.push_back(a);
-            }
-
-            values[1] = noun;
-            values[2] = verb;
-
-            int i{0};
-            while (i < values.size()) {
-
-                auto v = values[i];
-                if (v == 1 || v == 2) {
-
-                    auto a = values[i + 1];
-                    auto b = values[i + 2];
-                    auto c = values[i + 3];
-
-                    auto va = values[a];
-                    auto vb = values[b];
-
-                    if (v == 1) {
-
-                        int s = va + vb;
-                        values[c] = s;
-
-                    } else {
-                        int s = va * vb;
-                        values[c] = s;
-                    }
-                    i += 4;
-                } else {
-                    if (v == 99) {
-                        i = 100000;
-                    } else {
-
-                        i++;
-                        i %= values.size();
-                    }
-                }
-            }
-
-            r = values[0];
+            auto r = machine.run(noun, verb);
 
             if (r == 19690720) {
                 log << 100 * noun + verb << endl;
