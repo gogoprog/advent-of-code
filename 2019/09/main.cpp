792415ceb36847160273de8b68389b9135ce1f03
Gauthier Billot <gogoprog@gmail.com>
2019-12-09 06:31:03 +0100

    2019 09

---
+++ 2019/09/main.cpp
@@ -0,0 +1,204 @@
+#include "../../common.h"
+
+using lli = long long int;
+
+struct Program : Map<lli, lli> {
+    void load(const String line) {
+        auto inputs = splitString(line, ',');
+
+        clear();
+
+        lli i=0;
+        for (auto &input : inputs) {
+            (*this)[i] = std::stoi(input);
+            i++;
+        }
+    }
+};
+
+struct Machine {
+    Program program;
+    lli cursor{0};
+    lli relativeBase;
+
+    void load(const Program &pgm) {
+        program = pgm;
+        cursor = 0;
+        relativeBase = 0;
+    }
+
+    lli &getParam(const lli pos, const String modes) {
+        char mode = modes[modes.length() - pos];
+
+        switch (mode) {
+            case '0': {
+                auto a = program[cursor + pos];
+                return program[a];
+            }
+            case '1': {
+                auto &a = program[cursor + pos];
+                return a;
+            }
+
+            case '2': {
+                auto a = program[cursor + pos];
+                return program[relativeBase + a];
+            }
+            default:
+                throw "shit";
+        }
+    }
+
+    lli run(const lli input) {
+
+        while (cursor < program.size()) {
+            auto inst = program[cursor];
+
+            auto ps = inst / 100;
+            auto i = inst - ps * 100;
+
+            String modes;
+
+            if (ps == 0) {
+                modes = "0000";
+            } else {
+                modes = std::to_string(ps);
+                modes = "0000" + modes;
+            }
+
+            switch (i) {
+                case 1:
+                case 2: {
+                    auto a = getParam(1, modes);
+                    auto b = getParam(2, modes);
+                    auto &c = getParam(3, modes);
+
+                    lli r;
+
+                    if (i == 1) {
+                        r = a + b;
+                    } else {
+                        r = a * b;
+                    }
+
+                    c = r;
+
+                    cursor += 4;
+
+                } break;
+
+                case 3: {
+                    auto &a = getParam(1, modes);
+                    a = input;
+
+                    cursor += 2;
+                    break;
+                }
+
+                case 4: {
+
+                    auto &a = getParam(1, modes);
+                    /* log << a << endl; */
+                    cursor += 2;
+                    return a;
+
+                } break;
+
+                case 5: {
+                    auto &a = getParam(1, modes);
+                    auto &b = getParam(2, modes);
+
+                    if (a != 0) {
+                        cursor = b;
+                    } else {
+                        cursor += 3;
+                    }
+
+                } break;
+                case 6: {
+                    auto &a = getParam(1, modes);
+                    auto &b = getParam(2, modes);
+
+                    if (a == 0) {
+                        cursor = b;
+                    } else {
+                        cursor += 3;
+                    }
+
+                } break;
+                case 7: {
+                    auto &a = getParam(1, modes);
+                    auto &b = getParam(2, modes);
+                    auto &c = getParam(3, modes);
+
+                    if (a < b) {
+                        c = 1;
+                    } else {
+                        c = 0;
+                    }
+
+                    cursor += 4;
+
+                } break;
+                case 8: {
+                    auto &a = getParam(1, modes);
+                    auto &b = getParam(2, modes);
+                    auto &c = getParam(3, modes);
+
+                    if (a == b) {
+                        c = 1;
+                    } else {
+                        c = 0;
+                    }
+
+                    cursor += 4;
+
+                } break;
+
+                case 9: {
+                    auto &a = getParam(1, modes);
+
+                    relativeBase += a;
+
+                    cursor += 2;
+
+                } break;
+
+                case 99:
+                    return -1;
+                    break;
+                default:
+                    cursor++;
+            }
+
+            cursor %= program.size();
+        }
+
+        return -1;
+    }
+};
+
+void process(const String filename) {
+    log << "Processing " << filename << endl;
+    auto lines = getFileLines(filename);
+    auto line = lines[0];
+
+    Program program;
+
+    program.load(line);
+    /* program.load("1102,34915192,34915192,7,4,7,99,0"); */
+
+    Machine machine;
+    machine.load(program);
+    auto r = machine.run(1);
+    log << r << endl;
+
+    machine.load(program);
+    auto r2 = machine.run(2);
+    log << r2 << endl;
+}
+
+int main() {
+    process("input.txt");
+    return 0;
+}
