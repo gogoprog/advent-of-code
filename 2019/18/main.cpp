7e0d4827a060ba5a44184cfa7e713a7b67e45332
Gauthier Billot <gogoprog@gmail.com>
2019-12-24 09:36:58 +0100

    2019 18

--- 2019/18/main.cpp
+++ 2019/18/main.cpp
@@ -1,4 +1,5 @@
 // I needed a hint to complete this one and the hint was: "A single 3-dimensional bfs"
+// Hint for part2: "active bot"
 
 #include "../../common.h"
 
@@ -54,8 +55,73 @@ struct Node {
     }
 };
 
+struct Id2 {
+    Array<Point, 4> positions;
+    ulong keys;
+
+    bool operator<(const Id2 &other) const {
+        if (positions[0] == other.positions[0]) {
+            if (positions[1] == other.positions[1]) {
+                if (positions[2] == other.positions[2]) {
+                    if (positions[3] == other.positions[3]) {
+                        return keys < other.keys;
+                    }
+                    return positions[3] < other.positions[3];
+                }
+                return positions[2] < other.positions[2];
+            }
+            return positions[1] < other.positions[1];
+        }
+        return positions[0] < other.positions[0];
+    }
+};
+
+struct Node2 {
+    Id2 id;
+    int steps;
+    int activeBot = -1;
+
+    inline bool walk(const int n, const Point &npos, PMap &map) {
+        char c = map[npos.y][npos.x];
+        bool can_walk = false;
+
+        activeBot = n;
+
+        switch (c) {
+            case '.':
+            case '@':
+                can_walk = true;
+
+            default:
+                if (c >= 'a' && c <= 'z') {
+                    can_walk = true;
+                    auto key = c - 'a';
+                    if (!(id.keys & (1 << key))) {
+                        activeBot = -1;
+                    }
+                    id.keys |= (1 << key);
+                } else if (c >= 'A' && c <= 'Z') {
+                    auto key = (c - 'A');
+                    can_walk = id.keys & (1 << key);
+                }
+        }
+
+        if (can_walk) {
+            id.positions[n] = npos;
+            ++steps;
+        }
+
+        return can_walk;
+    }
+
+    bool operator<(const Node2 &other) const {
+        return steps < other.steps;
+    }
+};
+
 struct Level {
     Point startPos;
+    Vector<Point> startPositions;
     uint totalKeys;
     PMap map;
 
@@ -70,6 +136,7 @@ struct Level {
             for (auto c : line) {
                 if (c == '@') {
                     startPos = {x, y};
+                    startPositions.push_back(startPos);
                 }
 
                 if (c >= 'a' && c <= 'z') {
@@ -82,8 +149,15 @@ struct Level {
 
         map = lines;
     }
-
     void run() {
+        if (startPositions.size() == 1) {
+            run1();
+        } else {
+            run2();
+        }
+    }
+
+    void run1() {
         PriorityQueue<Node> q;
 
         Node n{{startPos, 0}};
@@ -132,6 +206,74 @@ struct Level {
 
         log << "part1: " << best << endl;
     }
+
+    void run2() {
+        int best{std::numeric_limits<int>::max()};
+        PriorityQueue<Node2> q;
+        Map<Id2, int> visited;
+
+        Node2 n{{{startPositions[0], startPositions[1], startPositions[2], startPositions[3]}, 0}};
+        q.push(n);
+
+        ulong targetKeys = 0;
+
+        for (uint k = 0; k < totalKeys; ++k) {
+            targetKeys |= (1 << k);
+        }
+
+        while (!q.empty()) {
+            const auto node{q.top()};
+            q.pop();
+
+            auto &vs = visited[node.id];
+
+            if (vs != 0 && vs <= node.steps) {
+                continue;
+            } else {
+                vs = node.steps;
+            }
+
+            if (node.steps < best) {
+                const auto &id = node.id;
+
+                if (node.activeBot == -1) {
+
+                    for (int n = 0; n < 4; n++) {
+                        for (const auto &delta : deltas) {
+                            const auto &npos = id.positions[n] + delta;
+                            auto copy = node;
+
+                            if (copy.walk(n, npos, map)) {
+
+                                if (copy.id.keys == targetKeys) {
+                                    best = copy.steps;
+                                } else {
+                                    q.push(copy);
+                                }
+                            }
+                        }
+                    }
+                } else {
+                    auto n = node.activeBot;
+                    for (const auto &delta : deltas) {
+                        const auto &npos = id.positions[n] + delta;
+                        auto copy = node;
+
+                        if (copy.walk(n, npos, map)) {
+
+                            if (copy.id.keys == targetKeys) {
+                                best = copy.steps;
+                            } else {
+                                q.push(copy);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        log << "part2: " << best << endl;
+    }
 };
 
 void process(const String filename) {
@@ -152,5 +294,9 @@ int main() {
     process("sample4.txt");
     process("sample5.txt");
     process("input.txt");
+    process("sample6.txt");
+    process("sample7.txt");
+    process("sample8.txt");
+    process("input2.txt");
     return 0;
 }
