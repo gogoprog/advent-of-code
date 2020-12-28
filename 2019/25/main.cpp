c4cd934af11bee5073d3c02d8a958dd089d4b990
Gauthier Billot <gogoprog@gmail.com>
2020-12-28 15:50:54 +0100

    2019 25

--- 2019/25/main.cpp
+++ 2019/25/main.cpp
@@ -1,4 +1,5 @@
 #include "../../common.h"
+#include <unistd.h>
 
 using lli = long long int;
 
@@ -192,17 +193,40 @@ struct Machine {
     }
 };
 
+using Item = String;
+
+enum Direction { N, E, S, W };
+
+struct Room {
+    String name;
+    Set<Item> items;
+    Array<Room *, 4> connections;
+};
+
 struct System {
     Program program;
     Machine machine;
+
+    Room *currentRoom;
+    Room *previousRoom{0};
+    Map<String, Room *> roomsMap;
+    Vector<Room> rooms;
+    Set<Item> pickedItems;
+
     Map<Point, char> map;
+    Map<Set<Item>, bool> triedItems;
     Point pos{0, 0};
+
+    int lastDirection{-1};
 
     void load(const String &line) {
         program.load(line);
         machine.load(program);
 
-        map[pos * 2] = '.';
+        rooms.reserve(128);
+        rooms.push_back({});
+
+        currentRoom = &rooms[0];
     }
 
     void run() {
@@ -219,8 +243,10 @@ struct System {
                 break;
 
             if (r == -60) {
-                draw();
-                log << "-> ";
+                /* log << "-> "; */
+
+                automate(outputs);
+                /*
                 if (inputs.empty()) {
                     String str;
                     std::getline(std::cin, str);
@@ -229,12 +255,16 @@ struct System {
                     log << inputs.front() << endl;
                     doInput(inputs.front());
                     inputs.pop_front();
-                }
+                }*/
+
+                outputs.clear();
             } else {
-                log << char(r);
+                /* log << char(r); */
                 outputs += char(r);
             }
         }
+
+        log << outputs << endl;
     }
 
     void doInput(const String line) {
@@ -258,7 +288,169 @@ struct System {
         map[(pos * 2 + before * 2) / 2] = ' ';
 
         map[pos * 2] = '.';
-        log << pos << endl;
+    }
+
+    void automate(const String outputs) {
+
+        enum State { DOORS, ITEMS, NONE };
+
+        auto getName = [](const int d) {
+            static String names[] = {"north", "east", "south", "west"};
+            return names[d];
+        };
+
+        auto getDirection = [](const String &input) -> Direction {
+            if (input == "east")
+                return E;
+            if (input == "west")
+                return W;
+            if (input == "north")
+                return N;
+            if (input == "south")
+                return S;
+
+            throw "Error";
+        };
+
+        auto lines = splitString(outputs);
+
+        State state{NONE};
+
+        currentRoom->items.clear();
+
+        for (auto line : lines) {
+            if (line[0] == '=' && line[1] == '=') {
+                if (currentRoom->name == "") {
+                    currentRoom->name = line;
+
+                } else {
+                    if (currentRoom->name != line) {
+                        throw "Error";
+                    }
+                }
+            } else if (line == "Doors here lead:") {
+                state = DOORS;
+            } else if (line == "Items here:") {
+                state = ITEMS;
+            } else if (line.find("ejected back to") != std::string::npos) {
+                currentRoom = previousRoom;
+                pos.y += 1;
+                lastDirection = -1;
+                triedItems[pickedItems] = true;
+            } else if (line == "") {
+                state = NONE;
+            } else {
+                String name = line;
+                name.erase(0, 2);
+                switch (state) {
+                    case DOORS: {
+                        auto d = getDirection(name);
+                        if (currentRoom->connections[d] == 0) {
+                            rooms.push_back({});
+                            rooms.back().connections[(d + 2) % 4] = currentRoom;
+                            currentRoom->connections[d] = &rooms.back();
+                        }
+                    } break;
+                    case ITEMS:
+                        currentRoom->items.insert(name);
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+        }
+
+        /* draw(); */
+
+        auto canPick = [&](const Item item) {
+            if (item == "infinite loop")
+                return false;
+            if (item == "photons")
+                return false;
+            if (item == "molten lava")
+                return false;
+            if (item == "escape pod")
+                return false;
+            if (item == "giant electromagnet")
+                return false;
+
+            return true;
+        };
+
+        auto canTry = [&](const Item item) {
+            auto copy = pickedItems;
+            copy.insert(item);
+            return !triedItems[copy];
+        };
+
+        auto mustMove = true;
+
+        if (triedItems[pickedItems]) {
+
+            if (currentRoom->name == "== Security Checkpoint ==") {
+                if (pickedItems.size() > 0) {
+                    if (rand() % 5 == 0) {
+                        doInput("drop " + *(pickedItems.begin()));
+                        pickedItems.erase(*pickedItems.begin());
+                        mustMove = false;
+                        /* log << pickedItems << endl; */
+                    }
+                }
+            }
+
+            if (mustMove && currentRoom->items.size() > 0) {
+                if (rand() % 5 == 0) {
+                    int p = rand() % currentRoom->items.size();
+                    auto item = *std::next(currentRoom->items.begin(), p);
+                    if (canPick(item)) {
+                        doInput("take " + item);
+                        pickedItems.insert(item);
+                        mustMove = false;
+                    }
+                }
+                /* log << pickedItems << endl; */
+            }
+        }
+
+        if (mustMove) {
+            int d{0};
+            int choice{-1};
+            int choices{0};
+            for (auto otherRoom : currentRoom->connections) {
+                if (otherRoom) {
+                    if (otherRoom->name == "") {
+                        choice = d;
+                    }
+                    choices++;
+                }
+                ++d;
+            }
+
+            if (choice == -1) {
+                do {
+                    choice = rand() % 4;
+                } while ((choices > 1 && choice == ((lastDirection + 2) % 4)) || currentRoom->connections[choice] == 0);
+            }
+
+            /* String str; */
+            /* std::getline(std::cin, str); */
+            /* usleep(10000); */
+
+            if (currentRoom->name == "== Security Checkpoint ==") {
+                if (triedItems[pickedItems]) {
+                    choice = 2;
+                } else {
+                    choice = 0;
+                    log << "trying: " << pickedItems << endl;
+                }
+            }
+
+            doInput(getName(choice));
+            lastDirection = choice;
+            previousRoom = currentRoom;
+            currentRoom = currentRoom->connections[choice];
+        }
     }
 
     void draw() {
