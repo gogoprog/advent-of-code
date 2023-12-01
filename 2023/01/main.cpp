f2bd72754b6222dcc99d55b67bc5bc278d351130
Gauthier Billot <gogoprog@gmail.com>
2023-12-01 12:54:42 +0100

    Code clean

--- 2023/01/main.cpp
+++ 2023/01/main.cpp
@@ -1,23 +1,15 @@
 #include "../../common.h"
 
 struct Context {
-    Strings lines;
-
-    void parse(const Strings &lines) {
-        this->lines = lines;
-    }
-
-    int compute(Strings spellings) {
-        auto result{0};
-
+    int compute(const Strings &lines, const Strings &spellings) {
         auto computeLineValue = [&](String line) {
-            auto view1 = rv::zip(rv::iota(0), spellings) | rv::transform([&](auto pair) {
-                             return std::pair{(pair.first % 9) + 1, line.find(pair.second)};
+            auto view1 = rv::zip(rv::iota(0), spellings) | rv::transform([&](const auto &pair) {
+                             return std::pair{pair.first, line.find(pair.second)};
                          }) |
                          rv::filter([&](auto pair) { return pair.second != String::npos; });
 
-            auto view2 = rv::zip(rv::iota(0), spellings) | rv::transform([&](auto pair) {
-                             return std::pair{(pair.first % 9) + 1, line.rfind(pair.second)};
+            auto view2 = rv::zip(rv::iota(0), spellings) | rv::transform([&](const auto &pair) {
+                             return std::pair{pair.first, line.rfind(pair.second)};
                          }) |
                          rv::filter([&](auto pair) { return pair.second != String::npos; });
 
@@ -25,21 +17,21 @@ struct Context {
 
             auto max = rs::max(view2, [](auto a, auto b) { return a.second < b.second; });
 
-            return min.first * 10 + max.first;
+            return ((min.first % 9) + 1) * 10 + (max.first % 9) + 1;
         };
 
         return rs::fold_left(lines | rv::transform(computeLineValue), 0, std::plus<int>());
     }
 
-    void part1() {
-        auto result = compute({"1", "2", "3", "4", "5", "6", "7", "8", "9"});
+    void part1(const Strings &lines) {
+        auto result = compute(lines, {"1", "2", "3", "4", "5", "6", "7", "8", "9"});
 
         log << "Part1: " << result << endl;
     }
 
-    void part2() {
-        auto result = compute({"one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "1", "2", "3",
-                               "4", "5", "6", "7", "8", "9"});
+    void part2(const Strings &lines) {
+        auto result = compute(lines, {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "1", "2",
+                                      "3", "4", "5", "6", "7", "8", "9"});
         log << "Part2: " << result << endl;
     }
 };
@@ -49,12 +41,10 @@ void process(const String filename) {
     auto lines = getFileLines(filename);
     {
         /* Context context; */
-        /* context.parse(lines); */
-        /* context.part1(); */
+        /* context.part1(lines); */
     } {
         Context context;
-        context.parse(lines);
-        context.part2();
+        context.part2(lines);
     }
 }
 
