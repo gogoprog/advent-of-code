71d3fdbdac0c826c3d178cf7621fa1959870eb61
Gauthier Billot <gogoprog@gmail.com>
2023-12-03 17:00:02 +0100

    Extreme speed

--- 2023/01/main.cpp
+++ 2023/01/main.cpp
@@ -1,7 +1,38 @@
-#include "../../common.h"
+/* #include "../../common.h" */
+
+#include <string_view>
+#include <ranges>
+#include <algorithm>
+
+namespace rs = std::ranges;
+namespace rv = std::ranges::views;
+
+using StringView = std::string_view;
+
+inline StringView getStringView(auto range) {
+    return StringView(&*range.begin(), rs::distance(range));
+}
+
+StringView getFileContent(const char *filename, const char delim = '\n') {
+
+    static char buffer[32 * 1024];
+    long length;
+    FILE *f = fopen(filename, "rb");
+
+    if (f) {
+        fseek(f, 0, SEEK_END);
+        length = ftell(f);
+        fseek(f, 0, SEEK_SET);
+        fread(buffer, sizeof(char), length, f);
+        fclose(f);
+    }
+    buffer[length] = '\0';
+
+    return StringView(buffer, length);
+}
 
 struct Context {
-    int computeP1(StringView lines) {
+    int computeP1(const StringView lines) {
         auto computeLineValue = [&](auto range) {
             auto line = getStringView(range);
             auto linelen = line.length();
@@ -10,17 +41,37 @@ struct Context {
 
             for (int c = 0; c < linelen; ++c) {
                 auto ch = *(line.begin() + c);
-                if (std::isdigit(ch)) {
-                    minval = ch - '0';
-                    break;
+                switch (ch) {
+                    case '1':
+                    case '2':
+                    case '3':
+                    case '4':
+                    case '5':
+                    case '6':
+                    case '7':
+                    case '8':
+                    case '9': {
+                        minval = ch - '0';
+                        c = linelen;
+                    } break;
                 }
             }
 
             for (int c = linelen - 1; c >= 0; --c) {
                 auto ch = *(line.begin() + c);
-                if (std::isdigit(ch)) {
-                    maxval = ch - '0';
-                    break;
+                switch (ch) {
+                    case '1':
+                    case '2':
+                    case '3':
+                    case '4':
+                    case '5':
+                    case '6':
+                    case '7':
+                    case '8':
+                    case '9': {
+                        maxval = ch - '0';
+                        c = -1;
+                    } break;
                 }
             }
 
@@ -32,113 +83,197 @@ struct Context {
         return rs::fold_left(rs::split_view(lines, '\n') | rv::transform(computeLineValue), 0, std::plus<int>());
     }
 
-    int computeP2(StringView lines, const Strings &spellings) {
-        auto len = spellings.size();
+    int computeP2(const StringView lines) {
 
-        /*
         auto computeLineValue = [&](auto range) {
-            auto line = StringView(&*range.begin(), rs::distance(range));
-
-            if (line.length() > 1) {
-                auto view1 = rv::zip(rv::iota(0), spellings) | rv::transform([&](const auto &pair) {
-                                 return std::pair{pair.first, line.find(pair.second)};
-                             }) |
-                             rv::filter([&](auto pair) { return pair.second != String::npos; });
-
-                auto view2 = rv::zip(rv::iota(0), spellings) | rv::transform([&](const auto &pair) {
-                                 return std::pair{pair.first, line.rfind(pair.second)};
-                             }) |
-                             rv::filter([&](auto pair) { return pair.second != String::npos; });
-
-                auto min = rs::min(view1, [](auto a, auto b) { return a.second < b.second; });
-
-                auto max = rs::max(view2, [](auto a, auto b) { return a.second < b.second; });
-
-                return ((min.first % 9) + 1) * 10 + (max.first % 9) + 1;
-            }
-            return 0;
-        };
-        */
-
-        auto computeLineValue3 = [&](auto range) {
             auto line = getStringView(range);
             auto linelen = line.length();
 
             int minval = 0, maxval = 0;
 
+            auto getChar = [&](auto index) { return *(line.begin() + index); };
+
             for (int c = 0; c < linelen; ++c) {
-                auto ch = *(line.begin() + c);
-                if (std::isdigit(ch)) {
-                    minval = ch - '0';
-                    break;
-                } else {
-                    for (int i = 0; i < len; ++i) {
-                        auto &spelling = *(spellings.begin() + i);
-                        if (line.substr(c, spelling.length()) == spelling) {
-                            minval = i + 1;
-                            break;
-                        }
+                auto ch = getChar(c);
+
+                auto is = [&](int i, char value) { return getChar(c + i + 1) == value; };
+
+                auto set = [&](int v) {
+                    minval = v;
+                    c = linelen;
+                };
+
+                auto accept = [&](auto n, char ch) {
+                    if (!is(n - 1, ch)) {
+                        return false;
                     }
 
-                    if (minval)
+                    c += n;
+
+                    return true;
+                };
+
+                switch (ch) {
+                    case '1':
+                    case '2':
+                    case '3':
+                    case '4':
+                    case '5':
+                    case '6':
+                    case '7':
+                    case '8':
+                    case '9': {
+                        minval = ch - '0';
+                        c = linelen;
+                    } break;
+
+                    case 'o':
+                        if (is(0, 'n') && is(1, 'e')) {
+                            set(1);
+                        }
+                        break;
+                    case 't':
+                        if (accept(1, 'w')) {
+                            if (is(0, 'o'))
+                                set(2);
+                        } else if (accept(1, 'h') && accept(1, 'r') && is(0, 'e') && is(1, 'e')) {
+                            set(3);
+                        }
+                        break;
+                    case 'f':
+                        if (is(0, 'o')) {
+                            if (accept(2, 'u') && accept(1, 'r'))
+                                set(4);
+                        } else if (accept(1, 'i') && accept(1, 'v') && is(0, 'e')) {
+                            set(5);
+                        }
+                        break;
+                    case 's':
+                        if (accept(1, 'i')) {
+                            if (accept(1, 'x'))
+                                set(6);
+                        } else if (is(0, 'e') && accept(2, 'v') && is(0, 'e') && is(1, 'n')) {
+                            set(7);
+                        }
+                        break;
+                    case 'e':
+                        if (accept(1, 'i') && accept(1, 'g') && accept(1, 'h') && is(0, 't')) {
+                            set(8);
+                        }
+                        break;
+                    case 'n':
+                        if (accept(1, 'i') && is(0, 'n') && is(1, 'e')) {
+                            set(9);
+                        }
                         break;
                 }
             }
 
             for (int c = linelen - 1; c >= 0; --c) {
-                auto ch = *(line.begin() + c);
-                if (std::isdigit(ch)) {
-                    maxval = ch - '0';
-                    break;
-                } else {
-                    for (int i = 0; i < len; ++i) {
-                        auto &spelling = *(spellings.begin() + i);
-                        auto slen = spelling.length();
-                        if (c >= slen && line.substr(c - slen + 1, slen) == spelling) {
-                            maxval = i + 1;
-                            break;
-                        }
+
+                auto ch = getChar(c);
+
+                auto is = [&](int i, char value) {
+                    if (c - i - 1 <= 0)
+                        return false;
+
+                    return getChar(c - i - 1) == value;
+                };
+
+                auto set = [&](int v) {
+                    maxval = v;
+                    c = -1;
+                };
+
+                auto accept = [&](auto n, char ch) {
+                    if (!is(n - 1, ch)) {
+                        return false;
                     }
-                    if (maxval)
-                        break;
+
+                    c -= n;
+                    return true;
+                };
+
+                switch (ch) {
+                    case '1':
+                    case '2':
+                    case '3':
+                    case '4':
+                    case '5':
+                    case '6':
+                    case '7':
+                    case '8':
+                    case '9': {
+                        maxval = ch - '0';
+                        c = -1;
+                    } break;
+
+                    case 'e': {
+                        if (is(0, 'n') && is(1, 'o')) {
+                            set(1);
+                        } else if (is(0, 'e') && is(1, 'r') && is(2, 'h') && is(3, 't')) {
+                            set(3);
+                        } else if (is(0, 'v') && is(1, 'i') && is(2, 'f')) {
+                            set(5);
+                        } else if (is(0, 'n') && is(1, 'i') && is(2, 'n')) {
+                            set(9);
+                        }
+                    } break;
+
+                    case 'o': {
+                        if (accept(1, 'w') && is(0, 't')) {
+                            set(2);
+                        }
+                    } break;
+
+                    case 'r': {
+                        if (accept(1, 'u') && is(0, 'o') && is(1, 'f')) {
+                            set(4);
+                        }
+                    } break;
+
+                    case 'x': {
+                        if (is(0, 'i') && is(1, 's')) {
+                            set(6);
+                        }
+                    } break;
+
+                    case 'n': {
+                        if (is(0, 'e') && is(1, 'v') && is(2, 'e') && is(3, 's')) {
+                            set(7);
+                        }
+                    } break;
+
+                    case 't': {
+                        if (is(0, 'h') && is(1, 'g') && is(2, 'i') && is(3, 'e')) {
+                            set(8);
+                        }
+                    } break;
                 }
             }
 
             auto v = minval * 10 + maxval;
 
-            /* log << line << " " << v << "\n"; */
-
             return v;
         };
 
-        return rs::fold_left(rs::split_view(lines, '\n') | rv::transform(computeLineValue3), 0, std::plus<int>());
+        return rs::fold_left(rs::split_view(lines, '\n') | rv::transform(computeLineValue), 0, std::plus<int>());
     }
 
     void part1(StringView lines) {
         auto result = computeP1(lines);
 
-        log << "Part1: " << result << endl;
+        printf("Part1: %d\n", result);
     }
 
     void part2(StringView lines) {
-        auto result = computeP2(lines, {
-                                           "one",
-                                           "two",
-                                           "three",
-                                           "four",
-                                           "five",
-                                           "six",
-                                           "seven",
-                                           "eight",
-                                           "nine",
-                                       });
-        log << "Part2: " << result << endl;
+        auto result = computeP2(lines);
+
+        printf("Part2: %d\n", result);
     }
 };
 
-void process(const String filename) {
-    log << "Processing " << filename << endl;
-    /* auto lines = getFileLines(filename); */
+void process(const char *filename) {
     auto content = getFileContent(filename);
     {
         Context context;
