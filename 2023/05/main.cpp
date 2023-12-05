3297065ccc80aeaa0224edd0facef9543bbd2727
Gauthier Billot <gogoprog@gmail.com>
2023-12-05 17:23:49 +0100

    Fastah

--- 2023/05/main.cpp
+++ 2023/05/main.cpp
@@ -23,14 +23,28 @@ struct Recipe {
 
         for (auto &range : ranges) {
 
-            if (range.size()) {
-                auto dest_start = range[0];
-                auto source_start = range[1];
-                auto length = range[2];
+            auto dest_start = *range.begin();
+            auto source_start = *(range.begin() + 1);
+            auto length = *(range.begin() + 2);
 
-                if (input >= source_start && input < source_start + length) {
-                    return dest_start + input - source_start;
-                }
+            if (input >= source_start && input < source_start + length) {
+                return dest_start + input - source_start;
+            }
+        }
+
+        return input;
+    }
+
+    ull unconvert(ull input) {
+
+        for (auto &range : ranges) {
+
+            auto dest_start = *range.begin();
+            auto source_start = *(range.begin() + 1);
+            auto length = *(range.begin() + 2);
+
+            if (input >= dest_start && input < dest_start + length) {
+                return source_start + input - dest_start;
             }
         }
 
@@ -41,7 +55,7 @@ struct Recipe {
 struct Context {
 
     Vector<ull> seeds;
-    Map<StringView, Recipe> recipes;
+    Vector<Recipe> recipes;
 
     void parse(auto content) {
         auto lines = rs::split_string_view(content, '\n');
@@ -75,13 +89,17 @@ struct Context {
             auto source = *split2.begin();
             auto dest = *(std::next(std::next(split2.begin())));
 
-            recipes[source].destination = dest;
+            auto &recipe = recipes.emplace_back();
+            recipe.destination = dest;
 
             for (int l = 1; l < block.size(); l++) {
                 Vector<ull> ints;
                 std::ranges::copy(rs::split_string_view(block[l], ' ') | rv::filter(filterEmpty) | rv::transform(toInt),
                                   std::back_inserter(ints));
-                recipes[source].ranges.push_back(ints);
+
+                if (ints.size()) {
+                    recipe.ranges.push_back(ints);
+                }
             }
         }
     }
@@ -91,15 +109,10 @@ struct Context {
 
         for (auto seed : seeds) {
 
-            StringView current = "seed";
             ull value = seed;
 
-            while (current != "location") {
-                auto &recipe = recipes[current];
-
+            for (auto &recipe : recipes) {
                 value = recipe.convert(value);
-
-                current = recipe.destination;
             }
 
             if (value < min) {
@@ -111,30 +124,37 @@ struct Context {
     }
 
     void part2(auto lines) {
+        auto is_seed = [&](auto input) {
+            for (int s = 0; s < seeds.size(); s += 2) {
+                auto start = seeds[s];
+                auto len = seeds[s + 1];
 
-        ull min{std::numeric_limits<ull>::max()};
-
-        for (int s = 0; s < seeds.size(); s += 2) {
-            for (ull seed = seeds[s]; seed < seeds[s] + seeds[s + 1]; ++seed) {
-
-                StringView current = "seed";
-                ull value = seed;
-
-                while (current != "location") {
-                    auto &recipe = recipes[current];
-
-                    value = recipe.convert(value);
-
-                    current = recipe.destination;
-                }
-
-                if (value < min) {
-                    min = value;
+                if (input >= start && input < start + len) {
+                    return true;
                 }
             }
-        }
 
-        log << "Part2: " << min << endl;
+            return false;
+        };
+
+        auto found = false;
+        ull i = 0;
+
+        while (true) {
+            ull value = i;
+
+            for (int r = recipes.size() - 1; r >= 0; r--) {
+                auto &recipe = recipes[r];
+                value = recipe.unconvert(value);
+            }
+
+            if (is_seed(value)) {
+                log << "Part2: " << i << endl;
+                break;
+            }
+
+            ++i;
+        }
     }
 };
 
