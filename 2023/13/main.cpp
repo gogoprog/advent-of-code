6d046bdf2cbd9f1b8672cac5d93f57755a8dabcf
Gauthier Billot <gogoprog@gmail.com>
2023-12-13 22:16:46 +0100

    2023 13

--- 2023/13/main.cpp
+++ 2023/13/main.cpp
@@ -58,6 +58,64 @@ auto find_symmetry = [](auto lines) {
     return 0;
 };
 
+auto find_symmetry2 = [](auto lines) {
+    auto width = lines[0].size();
+    auto height = lines.size();
+
+    for (int x = 1; x < width; ++x) {
+        int i = 0;
+
+        int diffs = 0;
+
+        while (x - i - 1 >= 0 && x + i < width) {
+
+            for (int y = 0; y < height; ++y) {
+                auto a = lines[y][x - i - 1];
+                auto b = lines[y][x + i];
+
+                if (a != b) {
+                    diffs++;
+                }
+            }
+
+            ++i;
+        }
+
+        if (diffs == 1) {
+            return x;
+        }
+    }
+
+    for (int y = 1; y < height; ++y) {
+        int i = 0;
+        int diffs = 0;
+
+        while (y - i - 1 >= 0 && y + i < height) {
+
+            for (int x = 0; x < width; ++x) {
+                auto a = lines[y - i - 1][x];
+                auto b = lines[y + i][x];
+
+                if (a != b) {
+                    diffs++;
+                }
+            }
+
+            ++i;
+        }
+
+        if (diffs == 1) {
+            return y * 100;
+        }
+    }
+
+
+    throw "oops";
+
+    return 0;
+};
+
+
 struct Context {
 
     void part1(auto content) {
@@ -74,16 +132,26 @@ struct Context {
 
             auto r = find_symmetry(lines);
             result += r;
-
-            log << part << endl;
-            log << r << endl;
         }
 
         log << "Part1: " << result << endl;
     }
 
-    void part2(auto lines) {
+    void part2(auto content) {
         auto result{0};
+
+        auto parts = content | rv::split_sv("\n\n");
+
+        for (auto part : parts) {
+
+            auto lines_view = part | rv::split_sv('\n');
+
+            StringViews lines;
+            rs::copy(lines_view, std::back_inserter(lines));
+
+            auto r = find_symmetry2(lines);
+            result += r;
+        }
 
         log << "Part2: " << result << endl;
     }
