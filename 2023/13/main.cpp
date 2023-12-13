037ac7f0059beda2c1d752b868f33d3dc2df08ef
Gauthier Billot <gogoprog@gmail.com>
2023-12-13 22:11:52 +0100

    2023 13.1

--- 2023/13/main.cpp
+++ 2023/13/main.cpp
@@ -1,9 +1,83 @@
 #include "../../common_fast.h"
 
+auto find_symmetry = [](auto lines) {
+    auto width = lines[0].size();
+    auto height = lines.size();
+
+    for (int x = 1; x < width; ++x) {
+        int i = 0;
+        bool good = true;
+
+        while (good && x - i - 1 >= 0 && x + i < width) {
+
+            for (int y = 0; y < height; ++y) {
+                auto a = lines[y][x - i - 1];
+                auto b = lines[y][x + i];
+
+                if (a != b) {
+                    good = false;
+                    break;
+                }
+            }
+
+            ++i;
+        }
+
+        if (good) {
+            return x;
+        }
+    }
+
+    for (int y = 1; y < height; ++y) {
+        int i = 0;
+        bool good = true;
+
+        while (good && y - i - 1 >= 0 && y + i < height) {
+
+            for (int x = 0; x < width; ++x) {
+                auto a = lines[y - i - 1][x];
+                auto b = lines[y + i][x];
+
+                if (a != b) {
+                    good = false;
+                    break;
+                }
+            }
+
+            ++i;
+        }
+
+        if (good) {
+            return 100 * y;
+        }
+    }
+
+
+    throw "oops";
+
+    return 0;
+};
+
 struct Context {
 
-    void part1(auto lines) {
+    void part1(auto content) {
         auto result{0};
+
+        auto parts = content | rv::split_sv("\n\n");
+
+        for (auto part : parts) {
+
+            auto lines_view = part | rv::split_sv('\n');
+
+            StringViews lines;
+            rs::copy(lines_view, std::back_inserter(lines));
+
+            auto r = find_symmetry(lines);
+            result += r;
+
+            log << part << endl;
+            log << r << endl;
+        }
 
         log << "Part1: " << result << endl;
     }
@@ -17,7 +91,7 @@ struct Context {
 
 void process(const char *filename) {
     log << "Processing " << filename << endl;
-    auto lines = rs::split_string_view(getFileContent(filename), '\n');
+    auto lines = getFileContent(filename);
     {
         Context context;
         context.part1(lines);
