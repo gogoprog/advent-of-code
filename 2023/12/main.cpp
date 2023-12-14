6b982101fe4a43107daedfc7be0a092e170ec5da
Gauthier Billot <gogoprog@gmail.com>
2023-12-14 16:48:52 +0100

    wip lost

--- 2023/12/main.cpp
+++ 2023/12/main.cpp
@@ -9,31 +9,6 @@ int64_t factorial(int64_t n) {
     return n * factorial(n - 1);
 }
 
-auto compute_groups = [](auto row, Ints &groups) {
-    int current = 0;
-
-    groups.resize(0);
-
-    auto check = [&]() {
-        if (current) {
-            groups.push_back(current);
-            current = 0;
-        }
-    };
-
-    for (auto c : row) {
-        switch (c) {
-            case '#':
-                current++;
-                break;
-            case '.':
-                check();
-                break;
-        }
-    }
-
-    check();
-};
 auto compute_groups2 = [](auto row, Ints &groups, const BitSet values, const int vlen) {
     int current = 0;
     int v = 0;
@@ -69,8 +44,50 @@ auto compute_groups2 = [](auto row, Ints &groups, const BitSet values, const int
     return true;
 };
 
-void doit(StringView row, const Ints &groups, BitSet values, const int vlen, int64_t &count) {
+auto find_biggest = [](auto row) {
+    int current = 0;
+    int best = 0;
 
+    auto check = [&]() {
+        if (current > best) {
+            best = current;
+            current = 0;
+        }
+    };
+
+    for (auto c : row) {
+        switch (c) {
+            case '#':
+                current++;
+                break;
+            default:
+                check;
+                break;
+        }
+    }
+
+    check();
+
+    return best;
+};
+
+void compute_all_possibilities(StringView row, Vector<Ints> &result, BitSet values, const int vlen) {
+    static Ints test_groups;
+    auto full = compute_groups2(row, test_groups, values, vlen);
+
+    if (full) {
+        result.push_back(test_groups);
+        return;
+    } else {
+        auto nv = values;
+        nv.set(vlen, true);
+        compute_all_possibilities(row, result, nv, vlen + 1);
+        nv.set(vlen, false);
+        compute_all_possibilities(row, result, nv, vlen + 1);
+    }
+}
+
+void doit(StringView row, const Ints &groups, BitSet values, const int vlen, int64_t &count) {
     Ints test_groups;
     auto full = compute_groups2(row, test_groups, values, vlen);
 
@@ -112,6 +129,66 @@ void doit(StringView row, const Ints &groups, BitSet values, const int vlen, int
     }
 };
 
+void try_chains(const Ints &target, const Vector<Map<Ints, int>> &possibilities, const int index, const Ints chain,
+                const int count, int64_t &result) {
+
+    if (chain == target) {
+
+        result += count;
+
+        return;
+    }
+
+    if (index < possibilities.size()) {
+
+        for (auto &map : possibilities[index]) {
+            Ints new_chain = chain;
+            new_chain.insert(new_chain.end(), map.first.begin(), map.first.end());
+            try_chains(target, possibilities, index + 1, new_chain, count * map.second, result);
+        }
+    }
+}
+
+void doit2(StringView row, const Ints &groups, BitSet values, const int vlen, int64_t &count) {
+
+    auto vparts = row | rv::split_sv('.') | rv::filter_empty;
+    StringViews parts;
+    rs::copy(vparts, std::back_inserter(parts));
+
+    /* log << parts << endl; */
+
+    Vector<Map<Ints, int>> possibilities;
+    possibilities.resize(parts.size());
+
+    auto p = 0;
+    for (auto part : parts) {
+
+        log << "part: " << part << endl;
+
+        for (int g = 0; g < groups.size(); g++) {
+            for (int gn = 0; g + gn <= groups.size(); gn++) {
+                Ints grs{groups.begin() + g, groups.begin() + g + gn};
+                int64_t subcount = 0;
+                doit(part, grs, {}, 0, subcount);
+
+                if (subcount) {
+                    auto &map = possibilities[p];
+                    map[grs] = subcount;
+                    /* log << subcount << " with " << grs << endl; */
+                }
+            }
+        }
+
+        /* result *= subcount; */
+        p++;
+    }
+
+    int64_t result = 0;
+    try_chains(groups, possibilities, 0, {}, 1, result);
+
+    count = result;
+};
+
 struct Context {
 
     void part1(auto lines) {
@@ -125,7 +202,7 @@ struct Context {
             rs::copy(groups_v, std::back_inserter(groups));
 
             int64_t count = 0;
-            doit(row, groups, {}, 0, count);
+            doit2(row, groups, {}, 0, count);
 
             return count;
         };
@@ -134,7 +211,7 @@ struct Context {
 
             auto r = compute_arrangements(line);
 
-            log << line << " : " << r << endl;
+            /* log << line << " : " << r << endl; */
 
             result += r;
         }
@@ -169,15 +246,18 @@ struct Context {
             }
 
             int64_t count = 0;
-            doit(final_row, final_groups, {}, 0, count);
+            doit2(final_row, final_groups, {}, 0, count);
 
             return count;
         };
 
         for (auto line : lines) {
+
+            log << "doing " << line << endl;
+
             auto r = compute_arrangements(line);
 
-            log << line << " : " << r << endl;
+            log << " : " << r << endl;
 
             result += r;
         }
@@ -191,12 +271,12 @@ void process(const char *filename) {
     {
         Context context;
         context.part1(lines);
-        context.part2(lines);
+        /* context.part2(lines); */
     }
 }
 
 int main() {
     process("sample.txt");
-    /* process("input.txt"); */
+    process("input.txt");
     return 0;
 }
