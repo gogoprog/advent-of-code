e4cebf234758f430d499a247de79d0c8a05735ad
Gauthier Billot <gogoprog@gmail.com>
2023-12-14 22:36:05 +0100

    wip

--- 2023/12/main.cpp
+++ 2023/12/main.cpp
@@ -3,29 +3,15 @@
 using Ints = Vector<int>;
 using BitSet = std::bitset<128>;
 
-int64_t factorial(int64_t n) {
-    if (n == 0 || n == 1)
-        return 1;
-    return n * factorial(n - 1);
-}
-
-auto compute_groups2 = [](auto row, Ints &groups, const BitSet values, const int vlen) {
+auto compute_group = [](auto row, const BitSet values, const int vlen, int &pos) {
     int current = 0;
     int v = 0;
-
-    groups.resize(0);
-
-    auto check = [&]() {
-        if (current) {
-            groups.push_back(current);
-            current = 0;
-        }
-    };
+    pos = 0;
 
     for (auto c : row) {
         if (c == '?') {
             if (v >= vlen) {
-                return false;
+                return -1;
             }
             c = values[v++] ? '#' : '.';
         }
@@ -34,159 +20,77 @@ auto compute_groups2 = [](auto row, Ints &groups, const BitSet values, const int
                 current++;
                 break;
             case '.':
-                check();
-                break;
-        }
-    }
-
-    check();
-
-    return true;
-};
-
-auto find_biggest = [](auto row) {
-    int current = 0;
-    int best = 0;
-
-    auto check = [&]() {
-        if (current > best) {
-            best = current;
-            current = 0;
-        }
-    };
-
-    for (auto c : row) {
-        switch (c) {
-            case '#':
-                current++;
-                break;
-            default:
-                check;
-                break;
-        }
-    }
-
-    check();
-
-    return best;
-};
-
-void compute_all_possibilities(StringView row, Vector<Ints> &result, BitSet values, const int vlen) {
-    static Ints test_groups;
-    auto full = compute_groups2(row, test_groups, values, vlen);
-
-    if (full) {
-        result.push_back(test_groups);
-        return;
-    } else {
-        auto nv = values;
-        nv.set(vlen, true);
-        compute_all_possibilities(row, result, nv, vlen + 1);
-        nv.set(vlen, false);
-        compute_all_possibilities(row, result, nv, vlen + 1);
-    }
-}
-
-void doit(StringView row, const Ints &groups, BitSet values, const int vlen, int64_t &count) {
-    Ints test_groups;
-    auto full = compute_groups2(row, test_groups, values, vlen);
-
-    if (full) {
-
-        if (test_groups == groups) {
-            count++;
-            return;
-        }
-
-        return;
-    }
-
-    bool cancel = false;
-
-    if (test_groups.size() < groups.size()) {
-
-        if (test_groups.size() > 1) {
-
-            for (int i = 0; i < test_groups.size() - 1; i++) {
-                if (test_groups[i] != groups[i]) {
-                    cancel = true;
-                    break;
+                if (current) {
+                    return current;
                 }
-            }
         }
-
-        if (!cancel) {
-            auto nv = values;
-            nv.set(vlen, true);
-            doit(row, groups, nv, vlen + 1, count);
-            nv.set(vlen, false);
-            doit(row, groups, nv, vlen + 1, count);
-        }
-    } else {
-        auto nv = values;
-        nv.set(vlen, false);
-        doit(row, groups, nv, vlen + 1, count);
+        pos++;
     }
+
+    return current;
 };
 
-void try_chains(const Ints &target, const Vector<Map<Ints, int>> &possibilities, const int index, const Ints chain,
-                const int count, int64_t &result) {
+void match(StringView row, const int group, BitSet values, const int vlen, Map<int, int> &counts) {
+    int pos;
+    auto test_group = compute_group(row, values, vlen, pos);
 
-    if (chain == target) {
+    if (test_group != -1) {
 
-        result += count;
+        if (test_group == group) {
+            counts[pos]++;
+        }
 
         return;
     }
 
-    if (index < possibilities.size()) {
+    auto nv = values;
+    nv.set(vlen, true);
+    match(row, group, nv, vlen + 1, counts);
+    nv.set(vlen, false);
+    match(row, group, nv, vlen + 1, counts);
+};
 
-        for (auto &map : possibilities[index]) {
-            Ints new_chain = chain;
-            new_chain.insert(new_chain.end(), map.first.begin(), map.first.end());
-            try_chains(target, possibilities, index + 1, new_chain, count * map.second, result);
-        }
-    }
-}
+void doit2(StringView row, int offset, const Ints &groups, int group_index, int count, int64_t &final_count) {
 
-void doit2(StringView row, const Ints &groups, BitSet values, const int vlen, int64_t &count) {
-
-    auto vparts = row | rv::split_sv('.') | rv::filter_empty;
-    StringViews parts;
-    rs::copy(vparts, std::back_inserter(parts));
-
-    /* log << parts << endl; */
-
-    Vector<Map<Ints, int>> possibilities;
-    possibilities.resize(parts.size());
-
-    auto p = 0;
-    for (auto part : parts) {
-
-        log << "part: " << part << endl;
-
-        for (int g = 0; g < groups.size(); g++) {
-            for (int gn = 0; g + gn <= groups.size(); gn++) {
-                Ints grs{groups.begin() + g, groups.begin() + g + gn};
-                int64_t subcount = 0;
-                doit(part, grs, {}, 0, subcount);
-
-                if (subcount) {
-                    auto &map = possibilities[p];
-                    map[grs] = subcount;
-                    /* log << subcount << " with " << grs << endl; */
-                }
-            }
-        }
-
-        /* result *= subcount; */
-        p++;
+    while (row[offset] == '.') {
+        offset++;
     }
 
-    int64_t result = 0;
-    try_chains(groups, possibilities, 0, {}, 1, result);
+    if (offset >= row.size())
+        return;
 
-    count = result;
+    /* for (int n = 1; offset + n <= row.size(); n++) { */
+
+    auto str = row.substr(offset);
+    /* log << str << endl; */
+    int64_t new_count = 0;
+
+    Map<int, int> counts;
+
+    match(str, groups[group_index], {}, 0, counts);
+
+    for (auto &kv : counts) {
+        /* log << kv.first << " " << kv.second << "\n"; */
+        auto ncount = count * kv.second;
+        if (group_index < groups.size() - 1) {
+            doit2(row, offset + kv.first + 1, groups, group_index + 1, ncount, final_count);
+        } else {
+            /* log << "yep!" << count << endl; */
+            final_count += ncount;
+        }
+    }
+    /* log << str << " | " << groups[group_index] << " = " << new_count << endl; */
+
+    /* if (new_count) { */
+    /*     count *= new_count; */
+    /*     if (group_index < groups.size() - 1) { */
+    /*         doit2(row, offset + n + 1, groups, group_index + 1, count, final_count); */
+    /*     } else { */
+    /*         /1* log << "yep!" << count << endl; *1/ */
+    /*         final_count += count; */
+    /*     } */
+    /* } */
+    /* } */
 };
 
 struct Context {
@@ -202,7 +106,7 @@ struct Context {
             rs::copy(groups_v, std::back_inserter(groups));
 
             int64_t count = 0;
-            doit2(row, groups, {}, 0, count);
+            doit2(row, 0, groups, 0, 1, count);
 
             return count;
         };
@@ -211,7 +115,7 @@ struct Context {
 
             auto r = compute_arrangements(line);
 
-            /* log << line << " : " << r << endl; */
+            log << line << " : " << r << endl;
 
             result += r;
         }
@@ -246,7 +150,7 @@ struct Context {
             }
 
             int64_t count = 0;
-            doit2(final_row, final_groups, {}, 0, count);
+            doit2(final_row, 0, final_groups, 0, 1, count);
 
             return count;
         };
@@ -271,12 +175,12 @@ void process(const char *filename) {
     {
         Context context;
         context.part1(lines);
-        /* context.part2(lines); */
+        context.part2(lines);
     }
 }
 
 int main() {
     process("sample.txt");
-    process("input.txt");
+    /* process("input.txt"); */
     return 0;
 }
