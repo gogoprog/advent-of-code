ea9d6a6b0ce93b4db2bd23cdd242f3480f966bb2
Gauthier Billot <gogoprog@gmail.com>
2023-12-19 08:43:58 +0100

    Bootstrap

---
+++ 2023/19/main.cpp
@@ -0,0 +1,32 @@
+#include "../../common_fast.h"
+
+struct Context {
+
+    void part1(auto lines) {
+        auto result{0};
+
+        log << "Part1: " << result << endl;
+    }
+
+    void part2(auto lines) {
+        auto result{0};
+
+        log << "Part2: " << result << endl;
+    }
+};
+
+void process(const char *filename) {
+    log << "Processing " << filename << endl;
+    auto lines = rs::split_string_view(getFileContent(filename), '\n');
+    {
+        Context context;
+        context.part1(lines);
+        context.part2(lines);
+    }
+}
+
+int main() {
+    process("sample.txt");
+    process("input.txt");
+    return 0;
+}
