3bcb1617652af09899891f57c26e9c5c16c5a20e
Gauthier Billot <gogoprog@gmail.com>
2023-12-22 15:30:38 +0100

    Impossibru

--- 2023/21/main.cpp
+++ 2023/21/main.cpp
@@ -65,7 +65,7 @@ template <int STEPS> struct Context {
         }
     }
 
-    void part1() {
+    int compute(const Vector<Coord> &starts, const int max_steps) {
         struct Node {
             Coord position;
             int steps;
@@ -75,7 +75,9 @@ template <int STEPS> struct Context {
         Map<Coord, Map<int, bool>> visited;
         Map<Coord, bool> solution;
 
-        q.push({start, 0});
+        for (auto start : starts) {
+            q.push({start, 0});
+        }
 
         while (!q.empty()) {
 
@@ -90,7 +92,7 @@ template <int STEPS> struct Context {
 
             v = true;
 
-            if (node.steps == STEPS) {
+            if (node.steps == max_steps) {
                 solution[node.position] = true;
                 continue;
             }
@@ -107,49 +109,24 @@ template <int STEPS> struct Context {
             }
         }
 
-        auto result = solution.size();
+        return solution.size();
+    }
+
+    void part1() {
+
+        auto result = compute({start}, STEPS);
 
         log << "Part1: " << result << endl;
     }
 
-    void part2(auto max_steps) {
-
-        log << "\npart2: " << max_steps << endl;
-
+    int computeMinSteps(const Coord &start, const Coord &end) {
         struct Node {
             Coord position;
             int steps;
         };
 
+        Map<Coord, bool> visited;
         Queue<Node> q;
-        Map<Coord, Map<int, bool>> visited;
-        Map<Coord, bool> visited2;
-        Map<Coord, bool> solution;
-
-        auto reachable = 0;
-
-        for (int y = 0; y < height; y++) {
-            for (int x = 0; x < width; x++) {
-                auto coord = Coord{x, y};
-                if (getChar(coord) != '#') {
-
-                    auto delta = coord - start;
-                    /* if ((delta.manhattan()% 2) == 0) */
-                    { reachable++; }
-                }
-            }
-        }
-
-        log << width << 'x' << height << ':' << reachable << endl;
-
-        auto ww = int(max_steps / width);
-        auto hh = int(max_steps / height);
-
-        auto r = ww * hh * reachable;
-
-        log << ww << "x" << hh << " -> " << r << '\n';
-
-        int64_t result = 0;
 
         q.push({start, 0});
 
@@ -158,7 +135,7 @@ template <int STEPS> struct Context {
             const auto node = q.front();
             q.pop();
 
-            auto &v = visited2[node.position];
+            auto &v = visited[node.position];
 
             if (v) {
                 continue;
@@ -166,33 +143,89 @@ template <int STEPS> struct Context {
 
             v = true;
 
-            if(node.position.x == 0) {
-                /* log << node.position << " -> " << node.steps << endl; */
-            }
-
-            if (node.steps == max_steps) {
-                /* log << node.position; */
-                continue;
+            if (node.position == end) {
+                return node.steps;
             }
 
             for (auto dir : dirs) {
-                {
-                    auto npos = node.position + dir;
+                auto npos = node.position + dir;
 
-                    if (isValid2(npos)) {
-                        auto copy = node;
-                        copy.position = npos;
-                        copy.steps++;
-                        q.push(copy);
+                if (isValid2(npos)) {
+                    auto copy = node;
+                    copy.position = npos;
+                    copy.steps++;
+                    q.push(copy);
+                }
+            }
+        }
+
+        return -1;
+    }
+
+    int64_t part2(auto max_steps) {
+
+        int64_t reachable = 0;
+
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                auto coord = Coord{x, y};
+                if (getChar(coord) != '#') {
+
+                    auto delta = coord - start;
+                    if ((delta.manhattan() % 2) != 0) {
+                        reachable++;
                     }
                 }
             }
         }
 
-        result = r;
-        result += visited.size() * 4 * (ww - 1);
+        log << width << 'x' << height << ':' << reachable << endl;
+
+        int64_t size = int(max_steps/ width);
+
+        log << "size= " << size << endl;
+
+        auto left = max_steps % size;
+
+        Point east{0, start.y};
+        Point west{width - 1, start.y};
+        Point north{start.x, 0};
+        Point south{start.x, height - 1};
+
+        int64_t reachables_from_ne = compute(Vector<Coord>{north, east}, left);
+        int64_t reachables_from_nw = compute({north, west}, left);
+        int64_t reachables_from_se = compute({south, east}, left);
+        int64_t reachables_from_sw = compute({south, west}, left);
+
+        log << "from ne " << reachables_from_ne << endl;
+        log << "from nw " << reachables_from_nw << endl;
+        log << "from se " << reachables_from_se << endl;
+        log << "from sw " << reachables_from_sw << endl;
+
+        int64_t full = 2 * size * size + 2 * size + 1;
+        int64_t result = full * reachable;
+
+        result += compute({north}, left);
+        result += compute({south}, left);
+        result += compute({east}, left);
+        result += compute({west}, left);
+        result += (size - 2) * reachables_from_ne;
+        result += (size - 2) * reachables_from_se;
+        result += (size - 2) * reachables_from_nw;
+        result += (size - 2) * reachables_from_sw;
+
+
+        /* int steps = computeMinSteps(start, {0, 0}); */
+        /* log << "steps = " << steps << '\n'; */
+
+        /* result = r; */
+        /* result += visited.size() * 4 * (ww - 1); */
+
+        /* result = compute(start, max_steps); */
 
         log << "Part2 (" << max_steps << ") : " << result << endl;
+
+        return result;
     }
 };
 
@@ -204,20 +237,39 @@ template <int steps> void process(const char *filename) {
         context.parse(lines);
         context.part1();
 
-        /* if constexpr (steps == 6) { */
-        /*     context.part2(6); */
-        /*     context.part2(10); */
-        /*     context.part2(50); */
-        /*     context.part2(100); */
-        /*     context.part2(500); */
-        /*     context.part2(1000); */
-        /*     context.part2(5000); */
-        /* } */
+        if constexpr (steps == 6) {
+            context.part2(6);
+            context.part2(10);
+            context.part2(50);
+            /* context.part2(100); */
+            /* context.part2(500); */
+            /* context.part2(1000); */
+            /* context.part2(5000); */
+        } else {
+            context.part2(26501365);
+        }
     }
 }
 
 int main() {
-    process<6>("sample.txt");
+    /* process<6>("sample.txt"); */
     process<64>("input.txt");
+
+    // too low      154779446780000
+    // too low      309558893560000
+    // too low      315902313510000
+
+    // incorrect
+    //              315903843707200
+    //              315903911283350
+    //              315905373904400
+    //              630905800837200
+    //              630907331034400
+    //              630916745267200
+    //              631810909228611
+    //             1263609254040000
+
+    // to test
+    //              630907439467200
     return 0;
 }
