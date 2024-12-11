de0c369c1861a35d3cc01664bfd265a090d6b476
Gauthier Billot <gogoprog@gmail.com>
2024-12-11 12:19:43 +0100

    2024 11

--- 2024/11/main.cpp
+++ 2024/11/main.cpp
@@ -1,8 +1,8 @@
 #include "../../common_fast.h"
 
-int countDigits(Int64 b) {
+inline int countDigits(Int64 b) {
     Int64 n = 10;
-    for (int i = 1; i < 24; ++i) {
+    for (int i = 1; i < 16; ++i) {
         if (n > b) {
             return i;
         }
@@ -10,6 +10,56 @@ int countDigits(Int64 b) {
         n *= 10;
     }
     return 0;
+}
+
+inline Int64 pow10(int n) {
+    Int64 result = 1;
+
+    for (int i = 0; i < 16; ++i) {
+        if (i >= n) {
+            return result;
+        }
+
+        result *= 10;
+    }
+
+    return 0;
+}
+
+static Int64 countNodesAfter(Int64 n, const int iterations) {
+    static Map<Pair<Int64, int>, Int64> cache;
+
+    Int64 result;
+
+    Pair<Int64, int> key{n, iterations};
+
+    auto entry = cache.find(key);
+
+    if (entry != cache.end()) {
+        return entry->second;
+    }
+
+    if (iterations == 0) {
+        return 1;
+    }
+
+    if (n == 0) {
+        result = countNodesAfter(1, iterations - 1);
+    } else if (int count = countDigits(n); (count & 1) == 0) {
+        Int64 sc = pow10(count / 2);
+        Int64 a = Int64(n / sc);
+        Int64 b = n - (a * sc);
+        auto totala = countNodesAfter(a, iterations - 1);
+        auto totalb = countNodesAfter(b, iterations - 1);
+        auto total = totala + totalb;
+        result = total;
+    } else {
+        result = countNodesAfter(n * 2024, iterations - 1);
+    }
+
+    cache[key] = result;
+
+    return result;
 }
 
 struct Context {
@@ -23,36 +73,19 @@ struct Context {
     void part1() {
         auto result{0_int64};
 
-        auto list = initList;
-
-        for (int i = 0; i < 25; ++i) {
-
-            for (auto it = list.begin(); it != list.end(); ++it) {
-
-                auto &n = *it;
-
-                if (n == 0) {
-                    n = 1;
-                } else if (int count = countDigits(n); (count & 1) == 0) {
-                    Int64 sc = std::pow(10, count / 2);
-                    Int64 a = Int64(n / sc);
-                    Int64 b = n - (a * sc);
-                    list.insert(it, a);
-                    auto back = list.insert(it, b);
-                    list.erase(it);
-                    it = back;
-                } else {
-                    n *= 2024;
-                }
-            }
+        for (auto n : initList) {
+            result += countNodesAfter(n, 25);
         }
 
-        result = list.size();
         log << "Part1: " << result << endl;
     }
 
     void part2() {
         auto result{0_int64};
+
+        for (auto n : initList) {
+            result += countNodesAfter(n, 75);
+        }
 
         log << "Part2: " << result << endl;
     }
