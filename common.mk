2a19814b162e9133d0cb945347e5acfbea3945a3
Gauthier Billot <gogoprog@gmail.com>
2024-12-25 22:18:32 +0100

    Cleaning

---
+++ common.mk
@@ -0,0 +1,27 @@
+
+OUTD = outd
+OUT = out
+OUTR = outr
+
+SRCS = ../../backward.cpp main.cpp
+
+CXX = g++
+CXXFLAGS = -std=c++23
+
+debug: $(OUTD)
+
+release: $(OUT)
+
+retail: $(OUTR)
+
+$(OUTD): $(SRCS)
+	$(CXX) $(CXXFLAGS) -g -DBACKWARD_HAS_BFD=1 $(SRCS) -o $@ -lbfd -ldl
+
+$(OUT): main.cpp
+	$(CXX) $(CXXFLAGS) main.cpp -o $@ -std=c++23 -O3 -g -static -static-libgcc -static-libstdc++ -fno-exceptions
+
+$(OUTR): main.cpp
+	$(CXX) $(CXXFLAGS) main.cpp -o $@ -std=c++23 -O3 -static -static-libgcc -static-libstdc++ -fno-exceptions
+
+clean:
+	rm -f $(OUTD) $(OUT) $(OUTR)
